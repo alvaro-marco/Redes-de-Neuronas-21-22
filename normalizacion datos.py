3ed80727a8b16383af78dd2642fc96bb33d9a533
bogdanandreibaltes <63234958+bogdanandreibaltes@users.noreply.github.com>
2021-09-17 16:35:22 +0200

    Archivo python para normalizar datos

---
+++ normalizacion datos.py
@@ -0,0 +1,6 @@
+import pandas as pd
+data = pd.read_csv('./concrete.dat2.csv', header=0)
+
+data["Cement"]=((data["Cement"]-data["Cement"].min())/(data["Cement"].max()-data["Cement"].min()))
+
+data.sample(5)
