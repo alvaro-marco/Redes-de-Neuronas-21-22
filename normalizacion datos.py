96f4879a6a294be1325b286b410990573b385376
bogdanandreibaltes <63234958+bogdanandreibaltes@users.noreply.github.com>
2021-09-17 16:40:37 +0200

    Datos normalizados para todo el dataset

--- normalizacion datos.py
+++ normalizacion datos.py
@@ -1,6 +1,8 @@
 import pandas as pd
 data = pd.read_csv('./concrete.dat2.csv', header=0)
 
-data["Cement"]=((data["Cement"]-data["Cement"].min())/(data["Cement"].max()-data["Cement"].min()))
+#data["Cement"]=((data["Cement"]-data["Cement"].min())/(data["Cement"].max()-data["Cement"].min()))
+
+dataf=((data-data.min())/(data.max()-data.min()))
 
 data.sample(5)
