2d2c83afa8c4ed9fb8480b2bc74e45c6aa66eb3d
bogdanandreibaltes <63234958+bogdanandreibaltes@users.noreply.github.com>
2021-09-26 19:55:01 +0200

    Implementación y experimentación (no funcionan)

---
+++ adaline_experimentacion.py
@@ -0,0 +1,42 @@
+import preparacion_datos
+import adaline
+
+# Load the data set
+
+bc = preparacion_datos.dataf
+
+X = bc.data
+
+y = bc.target
+
+#
+
+# Create training and test split
+
+#
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
+#
+
+# Instantiate CustomPerceptron
+
+#
+
+adaline = adaline.CustomAdaline(n_iterations = 10)
+
+#
+
+# Fit the model
+
+#
+
+adaline.fit(X_train, y_train)
+
+#
+
+# Score the model
+
+#
+
+adaline.score(X_test, y_test)
